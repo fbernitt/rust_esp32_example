0ef49889ba017d7f431b0af6793bd0f907c25dce
Folker Bernitt <folker-bernitt-github@gmx.de>
2020-05-27 16:59:13 +0200

    Add log support

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,18 @@
 #![no_main]
 
 use esp_idf_sys as idf;
+use esp_idf_logger;
+use log;
 
 #[no_mangle]
 pub fn app_main() {
+    esp_idf_logger::init().unwrap();
+    log::info!("Hello with logger");
+
+    // log manually
     unsafe {
-        
+      let text = b"Hello World\n\0";
+      idf::printf(text.as_ptr() as *const _);
     }
 }
 
