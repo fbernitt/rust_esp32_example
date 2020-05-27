6130dfde437e0f18f871fc2e1aa76846d9472a7b
Folker Bernitt <folker-bernitt-github@gmx.de>
2020-05-27 17:05:06 +0200

    Blink LED

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use esp_idf_sys as idf;
 use esp_idf_logger;
 use log;
 
+const LED : u32 = 2;
+const TICK_PERIOD_MS : idf::TickType_t = 1000 / idf::configTICK_RATE_HZ;
+
 #[no_mangle]
 pub fn app_main() {
     esp_idf_logger::init().unwrap();
@@ -14,6 +17,21 @@ pub fn app_main() {
     unsafe {
       let text = b"Hello World\n\0";
       idf::printf(text.as_ptr() as *const _);
+
+      // enable LED
+      idf::gpio_set_direction(LED, idf::GPIO_MODE_DEF_OUTPUT);
+    }
+
+    let mut led_on : bool = true;
+
+    loop {
+        enable_status_led(led_on);
+        if led_on {
+            led_on = false;
+        } else {
+            led_on = true;
+        }
+	sleep(1000);
     }
 }
 
@@ -25,5 +43,21 @@ extern "C" {
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     unsafe {
         abort();
+    }
+}
+
+fn enable_status_led(enable : bool) {
+  unsafe {
+    if enable {
+      idf::gpio_set_level(LED, 1);
+    } else {
+      idf::gpio_set_level(LED, 0);
+    }
+  }
+}
+
+fn sleep(duration_ms : u32) {
+    unsafe {
+      idf::vTaskDelay(duration_ms / TICK_PERIOD_MS);
     }
 }
